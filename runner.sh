84bbdf0403a4a13e089e4b1ae67164237002b245
apeckham <apeckham@gmail.com>
2014-08-13 19:53:56 -0700

    make SLEEP a config var

--- runner.sh
+++ runner.sh
@@ -3,5 +3,5 @@ erb config/redshift.yml.erb >config/config.yml
 while :
 do
   bundle exec bin/snowplow-storage-loader -c config/config.yml --skip ${RUNNER_SKIP:-analyze}
-  sleep 86400
+  sleep ${SLEEP:-86400}
 done
