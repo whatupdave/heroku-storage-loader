90f42c75165345df140a796a903f212acf7fd39b
Dave Newman <dave@snappyco.de>
2015-03-24 15:38:20 -0700

    run on schedule rather than sleep

--- runner.sh
+++ runner.sh
@@ -1,7 +1,2 @@
 erb config/redshift.yml.erb >config/config.yml
-
-while :
-do
-  bundle exec bin/snowplow-storage-loader -c config/config.yml --skip ${RUNNER_SKIP:-analyze}
-  sleep ${SLEEP:-86400}
-done
+bundle exec bin/snowplow-storage-loader -c config/config.yml --skip ${RUNNER_SKIP:-analyze}
